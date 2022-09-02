f0ede33a3abc3f08c268aee7149791ea91da379a
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-02 14:13:30 +0530

    Time: 1 ms (7.93%), Space: 42.6 MB (69.53%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,15 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        // int n = matrix.length;
-        // for (int i = 0; i < (n + 1) / 2; i ++) {
-        //     for (int j = 0; j < n / 2; j++) {
-        //         int temp = matrix[n - 1 - j][i];
-        //         matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-        //         matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
-        //         matrix[j][n - 1 - i] = matrix[i][j];
-        //         matrix[i][j] = temp;
-        //     }
-        // }
+        
         int m=matrix.length;
         int[][] dp = new int[m][m];
         
@@ -18,6 +9,7 @@ class Solution {
                 dp[i][j]=matrix[m-1-j][i];
             }
         }
+    
         for(int i=0;i<m;i++){
             for(int j=0;j<m;j++){
                 matrix[i][j]=dp[i][j];
