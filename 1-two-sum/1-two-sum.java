ed0068fce64e44caede9a1bd930a9cc159a4aeb5
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-06 09:11:58 +0530

    Time: 44 ms (46.54%), Space: 44.5 MB (82.13%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         int p=0,q=0;
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){// writer Khubchand Bansal
+                if(nums[i]+nums[j]==target){
                     p=i;
                     q=j;
                     break;
