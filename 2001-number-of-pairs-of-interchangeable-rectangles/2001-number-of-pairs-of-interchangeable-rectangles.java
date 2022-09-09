47dcc7565d03b08a4fff30b304ef025c27fd98ab
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-09 10:43:26 +0530

    Time: 164 ms (16.06%), Space: 153.2 MB (61.47%) - LeetHub

---
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long interchangeableRectangles(int[][] rectangles) {
+        Map<Double,Long>  map = new HashMap<>();
+        long sum=0;
+        for(int[] i:rectangles){
+            double k = (double)i[0]/i[1];
+            if(map.containsKey(k)){
+                long value=map.get(k)+(long)1;
+                map.put(k,value);
+                // sum+=(long)value;
+            }
+            else{
+                map.put(k,(long)1);
+            }
+        }
+        Iterator hmIterator = map.entrySet().iterator();
+        while (hmIterator.hasNext()) {
+ 
+            Map.Entry mapElement= (Map.Entry)hmIterator.next();
+            long n = (long)mapElement.getValue();
+            sum+=(n*(n-1)/2);
+        }
+        return sum;
+        
+        
+    }
+}
