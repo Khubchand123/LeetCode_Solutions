07e69453c6e21efae36e9343156268326edc1663
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-13 15:37:01 +0530

    Time: 2 ms (86.06%), Space: 43.6 MB (61.71%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.java
+++ 414-third-maximum-number/414-third-maximum-number.java
@@ -4,29 +4,15 @@ class Solution {
         int max1=Integer.MIN_VALUE;
         int max2=Integer.MIN_VALUE;
         int max3=Integer.MIN_VALUE;
-        int c1=0;
-        int c2=0;
         int c3=0;
         for(int i=0;i<n;i++){
             if(max1<=nums[i]){
                 max1=nums[i];
-                if(nums[i]==Integer.MIN_VALUE){
-                    c1=1;
-                }
-                else{
-                    c1=0;
-                }
             }
         }
         for(int i=0;i<n;i++){
             if(max2<nums[i] && nums[i]!=max1){
                 max2=nums[i];
-                if(nums[i]==Integer.MIN_VALUE){
-                    c2=1;
-                }
-                else{
-                    c2=0;
-                }
             }
         }
         for(int i=0;i<n;i++){
