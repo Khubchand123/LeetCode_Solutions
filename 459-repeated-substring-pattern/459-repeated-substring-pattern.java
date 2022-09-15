f0cd2077fb9771967e5b3f3b669429fc9c9c5f87
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-15 14:26:23 +0530

    Time: 547 ms (13.32%), Space: 380.7 MB (8.25%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        
+        String s1=s.substring(0,1);
+        for(int i=1;i<s.length();i++){
+           if(i+s1.length()>s.length()){
+              return false;
+           }
+           else if(s.substring(i,i+s1.length()).equals(s1)){
+                int j=i;
+                while(i<s.length() && (i+s1.length()<=s.length())){
+                    if(s.substring(i,i+s1.length()).equals(s1)){
+                        i+=s1.length();
+                    }
+                    else{
+                        
+                       break;
+                    }
+                }
+              if(i!=s.length()){
+                 i=j;
+                 s1+=s.substring(i,i+1);
+              }
+            }
+            else{
+                s1+=s.substring(i,i+1);
+            }
+        }
+        if(s.length()==s1.length()){
+            return false;
+        }
+        return true;
+    }
+}
