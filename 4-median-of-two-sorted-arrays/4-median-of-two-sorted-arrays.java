08147a52eebc8f4e6680529d47676fe05c306872
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-18 19:45:42 +0530

    Time: 2 ms (100.00%), Space: 43.4 MB (90.57%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -0,0 +1,68 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int total=n1+n2;
+        if(n1==0){
+            if(n2%2==0){
+                return (nums2[total/2-1]+nums2[total/2])/2.0;
+            }
+            else{
+                return (nums2[total/2])*1.0;
+            }
+        }
+        if(n2==0){
+            if(n1%2==0){
+                return (nums1[total/2-1]+nums1[total/2])/2.0;
+            }
+            else{
+                return (nums1[total/2])*1.0;
+            }
+        }
+        int x=0;
+        int i=0;
+        int j=0;
+        int num1=0;
+        int num2=0;
+        while(x<=total/2) {
+            num2=num1;
+			if(j==nums2.length && i!=nums1.length) {
+				num1=nums1[i];
+				i++;
+				x++;
+			}else if(i==nums1.length && j!=nums2.length) {
+				num1=nums2[j];
+				j++;
+				x++;
+			}else if(nums1[i]<nums2[j]) {
+				num1=nums1[i];
+				i++;
+				x++;
+			}else if(nums2[j]<nums1[i]) {
+				num1=nums2[j];
+				j++;
+				x++;
+			}else {
+				num1=nums2[j];
+				j++;
+				x++;
+                if(x>total/2){
+                    break;
+                }
+                num2=num1;
+				num1=nums1[i];
+				i++;
+				x++;
+			}
+		}
+		if(total%2==0) {
+			
+			return (double)((double)num1+(double)num2)/2;
+		}else {
+			return (double)num1;
+		}
+	
+        
+        
+    }
+}
