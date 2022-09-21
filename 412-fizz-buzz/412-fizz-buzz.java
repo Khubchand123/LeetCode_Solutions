f8e6f7eeccb9c4623a1f2e6f6c842988f529c91d
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-21 19:54:45 +0530

    Time: 2 ms (77.30%), Space: 48.1 MB (66.09%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> ans = new ArrayList<>();
+        
         for(int i=1;i<=n;i++){
             if(i%3==0){
                 if(i%5==0){
