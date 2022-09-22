d40d7e651d060cc74379e73092381b5067259eb4
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-22 21:07:41 +0530

    Time: 173 ms (44.62%), Space: 54.3 MB (92.84%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,29 +1,46 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        
+        // Approach 1 By using Stack
+        
         int n=temperatures.length;
-        int[] ans = new int[n];
-        for(int i=0;i<n-1;i++){
-            int count=0;
-            int p=0;
-            for(int j=i+1;j<n;j++){
-                if(temperatures[j]>temperatures[i]){
-                    p=1;
-                    count++;
-                    break;
-                }
-                else{
-                    count++;
-                }
-                
+        int ans[]=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int j=0;j<n;j++){
+            while(!st.isEmpty() && temperatures[j]>temperatures[st.peek()]){
+                ans[st.peek()]=j-st.peek();
+                st.pop();
             }
-            if(p==1){
-                ans[i]=count;
-            }
-            else{
-                ans[i]=0;
-            }
+            st.push(j);
         }
-        ans[n-1]=0;
         return ans;
+        
+        
+        // Approach 2 Bruth Force Algorithm
+//         int n=temperatures.length;
+//         int[] ans = new int[n];
+//         for(int i=0;i<n-1;i++){
+//             int count=0;
+//             int p=0;
+//             for(int j=i+1;j<n;j++){
+//                 if(temperatures[j]>temperatures[i]){
+//                     p=1;
+//                     count++;
+//                     break;
+//                 }
+//                 else{
+//                     count++;
+//                 }
+                
+//             }
+//             if(p==1){
+//                 ans[i]=count;
+//             }
+//             else{
+//                 ans[i]=0;
+//             }
+//         }
+//         ans[n-1]=0;
+//         return ans;
     }
 }
