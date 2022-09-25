b1e6a6dd56fc3512f5ee811760238f6e9db50723
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-25 21:40:17 +0530

    Time: 7 ms (61.98%), Space: 44 MB (52.99%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,20 +1,50 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        int n=s.length();
-        String s1="";
-        String s2="";
-        for(int i=0;i<n;i++){
-            if((s.charAt(i)>='0' && s.charAt(i)<='9') || (s.charAt(i)>='a' && s.charAt(i)<='z')){
-                s1+=s.substring(i,i+1);
+        int l = 0;
+        int r = s.length() - 1;
+        s = s.toLowerCase();
+        while(l <= r) {
+            char lChar = s.charAt(l);
+            char rChar = s.charAt(r);
+            
+            if(!isValid(lChar)) {
+                l++;
+                continue;
             }
-            if((s.charAt(n-i-1)>='0' && s.charAt(n-i-1)<='9') || (s.charAt(n-i-1)>='a' && s.charAt(n-i-1)<='z')){
-                s2+=s.substring(n-i-1,n-i);
+            if(!isValid(rChar)) {
+                r--;
+                continue;
             }
+            
+            if(lChar != rChar) return false;
+            l++;
+            r--;
         }
-        if(s1.equals(s2)){
+        
+        return true;
+    }
+    
+    static boolean isValid(Character ch) {
+        if((ch >= 48 && ch <= 57) || (ch >= 97 && ch <= 122))
             return true;
-        }
+        
         return false;
     }
+        // s=s.toLowerCase();
+        // int n=s.length();
+        // String s1="";
+        // String s2="";
+        // for(int i=0;i<n;i++){
+        //     if((s.charAt(i)>='0' && s.charAt(i)<='9') || (s.charAt(i)>='a' && s.charAt(i)<='z')){
+        //         s1+=s.substring(i,i+1);
+        //     }
+        //     if((s.charAt(n-i-1)>='0' && s.charAt(n-i-1)<='9') || (s.charAt(n-i-1)>='a' && s.charAt(n-i-1)<='z')){
+        //         s2+=s.substring(n-i-1,n-i);
+        //     }
+        // }
+        // if(s1.equals(s2)){
+        //     return true;
+        // }
+        // return false;
+    
 }
