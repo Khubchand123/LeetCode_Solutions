48f7571ebb98b6a6ffc6542999a95925cf35d35d
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-09-29 11:46:02 +0530

    Time: 37 ms (11.46%), Space: 55.1 MB (14.13%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        HashMap<Character,Character> map = new HashMap<>();
+        int count=0;
+        for(int i=0;i<key.length();i++){
+            if(key.charAt(i)!=' ' && !map.containsKey(key.charAt(i))){
+                map.put(key.charAt(i),(char)(97+count));
+                count++;
+            }
+        }
+        String s1="";
+        for(int i=0;i<message.length();i++){
+            if(message.charAt(i)!=' '){
+                char c=map.get(message.charAt(i));
+                s1+=String.valueOf(c);
+            }
+            else{
+                s1+=" ";
+            }
+        }
+        return s1;
+    }
+}
