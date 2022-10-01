927f0a23033fb4c53541ad1c7cd0bc2a16727427
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-01 09:47:39 +0530

    Time: 10 ms (21.89%), Space: 42.1 MB (12.64%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        int n=columnNumber;
+        String s="";
+        while(n>0){
+            char c='A';
+            if(n%26==0){
+                c='Z';
+                n=n/26-1;
+            }
+            else{
+                int a=64+n%26;
+                c=(char)a;
+                n=n/26;
+            }
+            
+            s=c+s;
+            
+        }
+        return s;
+    }
+}
