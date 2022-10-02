173c4c44c63e8ce6f6a0fc0a363d3506b3f5e380
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-02 23:09:38 +0530

    Time: 7 ms (29.92%), Space: 42.2 MB (34.75%) - LeetHub

---
+++ 728-self-dividing-numbers/728-self-dividing-numbers.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> obj = new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            int num=i;
+            List<Integer> temp = new ArrayList<>();
+            int sign=0;
+            while(num>0){
+                if(num%10==0){
+                    sign=1;
+                    break;
+                }
+                temp.add(num%10);
+                num=num/10;
+            }
+            int sign1=0;
+            if(sign==0){
+                for(int j:temp){
+                    if(i%j!=0){
+                        sign1=1;
+                        break;
+                    }
+                }
+                if(sign1==0){
+                    obj.add(i);
+                }
+            }
+            
+        }
+        return obj;
+    }
+}
