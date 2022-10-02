49f12b44163a0f1116105fe99df6fa1fb1a24556
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-02 22:47:06 +0530

    Time: 3 ms (60.64%), Space: 40.9 MB (43.94%) - LeetHub

--- 507-perfect-number/507-perfect-number.java
+++ 507-perfect-number/507-perfect-number.java
@@ -1,17 +1,31 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum=0;
-        if(num%2!=0){
+//         int sum=0;
+//         if(num%2!=0){
+//             return false;
+//         }
+//         else{
+//             for(int i=1;i<=num/2;i++){
+//                 if(num%i==0){
+//                     sum+=i;
+//                 }
+//             }
+//         }
+        
+//         return sum==num;
+        if (num <= 0) {
             return false;
         }
-        else{
-            for(int i=1;i<=num/2;i++){
-                if(num%i==0){
-                    sum+=i;
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
                 }
+
             }
         }
-        
-        return sum==num;
+        return sum - num == num;
     }
 }
