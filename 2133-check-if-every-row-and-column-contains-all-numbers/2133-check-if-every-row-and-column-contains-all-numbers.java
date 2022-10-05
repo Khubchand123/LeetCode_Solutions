3f23b1133fa42c733c0ff8b36386bde7a8c446c0
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-05 17:48:46 +0530

    Time: 13 ms (76.72%), Space: 68.5 MB (77.01%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
         int n=matrix.length;
-        
-        // int sum=(n*(n+1))/2;
         for(int i=0;i<n;i++){
             
             int arr[]=new int[n];
