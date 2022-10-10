291c039e56706526bdac922e4c46f8ade3d48f11
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-10 12:43:08 +0530

    Time: 1 ms (96.97%), Space: 42 MB (94.04%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -13,46 +13,5 @@ class Solution {
         }
         return true;
     }
-//         List<String> ans = new ArrayList<>();
-//         pattern = countChar(pattern);
-//         for(String word:words){
-//             if(matchChar(pattern,countChar(word))){
-//                 ans.add(word);
-//             }
-//         }
-//         return ans;
-        
-//     }
-    
-//     public static String countChar(String s){
-//         int k=1;
-//         if(s.length()==1){
-//             return s+"1";
-//         }
-//         String ans="";
-//         for(int i=0;i<s.length()-1;i++){
-//             if(s.charAt(i)==s.charAt(i+1)){
-//                 k++;
-//             }
-//             else{
-//                 ans+=s.substring(i,i+1)+Integer.toString(k);
-//                 k=1;
-//             }
-//         }
-//         if(s.charAt(s.length()-2)==s.charAt(s.length()-1)){
-//             ans+=s.substring(s.length()-1,s.length())+Integer.toString(k);
-//         }
-//         else{
-//             ans+=s.substring(s.length()-1,s.length())+"1";
-//         }
-//         return ans;
-//     }
-//     public static boolean matchChar(String s1,String s2){
-//         for(int i=1;i<s1.length();i+=2){
-//             if(s1.charAt(i)!=s2.charAt(i)){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
+
 }
