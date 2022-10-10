22be6f438b7a38e9b922ef0a62ee57784a56c6ef
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-10 11:57:26 +0530

    Time: 6 ms (12.97%), Space: 41.7 MB (75.31%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,16 +1,16 @@
 class Solution {
     public String breakPalindrome(String palindrome) {
-        if(palindrome.length()==1){
+        int n=palindrome.length();
+        if(n==1){
             return "";
         }
         
-        
-        for(int i=0;i<palindrome.length()/2;i++){
+        for(int i=0;i<n/2;i++){
             if(palindrome.charAt(i)!='a'){
-                return palindrome.substring(0,i)+"a"+palindrome.substring(i+1,palindrome.length());
+                return palindrome.substring(0,i)+"a"+palindrome.substring(i+1,n);
             }
         }
-        return palindrome.substring(0,palindrome.length()-1)+"b";
         
+        return palindrome.substring(0,n-1)+"b";
     }
 }
