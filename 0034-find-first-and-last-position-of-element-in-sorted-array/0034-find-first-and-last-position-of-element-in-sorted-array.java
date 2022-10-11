043e22aa830f82297f28cd3117f9dfb72470f3c5
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-11 20:51:51 +0530

    Time: 1 ms (44.45%), Space: 47 MB (71.86%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -21,24 +21,22 @@ class Solution {
             mid=(left+right)/2;
         }
         int[] arr = new int[2];
-        
+             // if element not present
         if(k==-1){
             arr[0]=-1;
             arr[1]=-1;
         }
         else{
             int a=k-1;
-            
+                // check upto where at first index target present
             while(a>=0 && nums[a]==target){
                 a--;
             }
-            
             int b=k+1;
-            
+               // check upto where at last index target present
             while(b<nums.length && nums[b]==target){
                 b++;
             }
-            
             arr[0]=a+1;
             arr[1]=b-1;
         }
