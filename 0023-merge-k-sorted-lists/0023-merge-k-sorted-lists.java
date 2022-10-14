33eeacb0d225511b7e5a3e311b71be91d691b42d
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-14 14:31:14 +0530

    Time: 9 ms (53.45%), Space: 49.2 MB (5.16%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,6 +11,7 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         List<Integer> obj = new ArrayList<>();
+            // adding all value in list
         for(ListNode temp:lists){
             while(temp!=null){
                 obj.add(temp.val);
@@ -20,9 +21,12 @@ class Solution {
         if(obj.size()==0){
             return null;
         }
+        
+            // sorting list
         Collections.sort(obj);
         ListNode ans = new ListNode(obj.get(0));
         ListNode temp=ans;
+            // add list element in new Listnode
         for(int i=1;i<obj.size();i++){
             temp.next = new ListNode(obj.get(i));
             temp=temp.next;
