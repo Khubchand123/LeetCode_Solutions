15223ef5aa8a880fe5c0cc4f3e79cce0cd10e198
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-18 22:24:13 +0530

    Time: 6 ms (44.29%), Space: 55.5 MB (40.00%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,19 +11,57 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+                // 1. Brute Force Algorithm
+//         ListNode temp1=headA;
+//         ListNode temp2=headB;
+//         while(temp1!=null){
+//             while(temp2!=null){
+//                 if(temp1==temp2){
+//                     return temp1;
+//                 }
+//                 temp2=temp2.next;
+//             }
+//             temp1=temp1.next;
+//             temp2=headB;
+//         }
+        
+//         return null;
+        
+        // 2. Best Approach
+        
+        ListNode temp=headA;
+        int sizeA=0;
+        while(temp!=null){
+            sizeA++;
+            temp=temp.next;
+        }
+        int sizeB=0;
+        temp=headB;
+        while(temp!=null){
+            sizeB++;
+            temp=temp.next;
+        }
+        if(sizeA>sizeB){
+            int dif=sizeA-sizeB;
+            while(dif--!=0){
+                headA=headA.next;
+            }
+        }
+        else{
+           int dif=sizeB-sizeA;
+           while(dif--!=0){
+               headB=headB.next;
+           } 
+        }
         ListNode temp1=headA;
         ListNode temp2=headB;
         while(temp1!=null){
-            while(temp2!=null){
-                if(temp1==temp2){
-                    return temp1;
-                }
-                temp2=temp2.next;
+            if(temp1==temp2){
+                return temp1;
             }
             temp1=temp1.next;
-            temp2=headB;
+            temp2=temp2.next;
         }
-        
-        return null;
+        return temp1;
     }
 }
