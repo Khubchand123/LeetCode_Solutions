1e39019877e269e102c551200ed82b967f5dcdbf
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-23 18:03:27 +0530

    Time: 2 ms (97.99%), Space: 55.1 MB (21.57%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int arr[] = new int[nums.length+1];
+        int ans[] = new int[2];
+        for(int i=0;i<nums.length;i++){
+            arr[nums[i]]++;
+        }
+        for(int i=1;i<nums.length+1;i++){
+            if(arr[i]==0){
+                ans[1]=i;
+            }
+            if(arr[i]==2){
+                ans[0]=i;
+            }
+        }
+        return ans;
+    }
+}
