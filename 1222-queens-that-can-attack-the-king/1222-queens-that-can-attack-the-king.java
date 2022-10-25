be839140374b0af4c2516eeefde744dc0b968bc3
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-25 17:04:18 +0530

    Time: 1 ms (99.25%), Space: 43.3 MB (67.79%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -7,6 +7,7 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         int i=king[0];
         int j=king[1];
+                 // checking 1st queen in left top diagonal
         while(i>=0 && j>=0){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
@@ -20,6 +21,7 @@ class Solution {
         }
         i=king[0];
         j=king[1];
+                 // checking 1st queen in right top diagonal
         while(i>=0 && j<8){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
@@ -33,6 +35,7 @@ class Solution {
         }
         i=king[0];
         j=king[1];
+                 // checking 1st queen in left down diagonal
         while(i<8 && j>=0){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
@@ -46,6 +49,7 @@ class Solution {
         }
         i=king[0];
         j=king[1];
+                 // checking 1st queen in right down diagonal
         while(i<8 && j<8){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
@@ -59,6 +63,7 @@ class Solution {
         }
         i=king[0];
         j=king[1];
+                 // checking 1st queen in left horizontally 
         while(i>=0){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
@@ -70,6 +75,7 @@ class Solution {
             i--;
         }
         i=king[0];
+                    // checking 1st queen in right horizontally 
         while(i<8){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
@@ -81,6 +87,7 @@ class Solution {
             i++;
         }
         i=king[0];
+            // checking 1st queen in top vertically 
         while(j>=0){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
@@ -92,6 +99,7 @@ class Solution {
             j--;
         }
         j=king[1];
+                // checking 1st queen in down vertically
         while(j<8){
             if(board[i][j]==1){
                 List<Integer> temp = new ArrayList<>();
