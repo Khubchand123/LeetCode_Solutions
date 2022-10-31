a203f5e35615d1717d3a3af0db4b2f12658afb0b
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-10-31 21:07:01 +0530

    Time: 1 ms (84.33%), Space: 43.1 MB (19.03%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,27 +14,42 @@
  * }
  */
 class Solution {
+           // 1. using O(n) Space
+    // void preOrder(List<Integer> obj,TreeNode root){
+    //     if(root==null){
+    //         return;
+    //     }
+    //     obj.add(root.val);
+    //     preOrder(obj,root.left);
+    //     preOrder(obj,root.right);
+    // }
+    // public void flatten(TreeNode root) {
+    //     if(root==null){
+    //         return;
+    //     }
+    //     List<Integer> obj = new ArrayList<>();
+    //     preOrder(obj,root);
+    //     root.left=null;
+    //     root.right=null;
+    //     TreeNode temp=root;
+    //     for(int i=1;i<obj.size();i++){
+    //         temp.right=new TreeNode(obj.get(i));
+    //         temp=temp.right;
+    //     }
+    // }
     
-    void preOrder(List<Integer> obj,TreeNode root){
-        if(root==null){
-            return;
-        }
-        obj.add(root.val);
-        preOrder(obj,root.left);
-        preOrder(obj,root.right);
-    }
+    
+            // without using extra space
+    TreeNode prev=null;
     public void flatten(TreeNode root) {
         if(root==null){
             return;
         }
-        List<Integer> obj = new ArrayList<>();
-        preOrder(obj,root);
+        flatten(root.right);
+        flatten(root.left);
         root.left=null;
-        root.right=null;
-        TreeNode temp=root;
-        for(int i=1;i<obj.size();i++){
-            temp.right=new TreeNode(obj.get(i));
-            temp=temp.right;
-        }
+        root.right=prev;
+        prev=root;
+        
     }
 }
