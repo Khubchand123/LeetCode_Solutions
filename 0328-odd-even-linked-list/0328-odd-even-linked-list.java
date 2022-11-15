99f64524db98116193adc34b875a025a43f8d04b
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2022-11-15 09:20:51 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (90.36%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,35 +10,53 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null || head.next==null || head.next.next==null){
+        
+        if(head==null){
             return head;
         }
-        ListNode head1=head;
-        ListNode head2=head.next;
-        ListNode temp1=head1;
-        ListNode temp2=head2;
-        ListNode slow=head;
-        int count=0;
-        while(slow!=null){
-            if(count%2==0){
-                if(temp1.next.next!=null){
-                    temp1.next=temp1.next.next;
-                    temp1=temp1.next;
-                }
-                count=1;
-            }
-            else{
-                temp2.next=temp2.next.next;
-                temp2=temp2.next;
-                count=0;
-            }
-            slow=slow.next;
-            
+        ListNode odd=head;
+        ListNode even = head.next;
+        ListNode evenhead=even;
+        while(even!=null && even.next!=null){
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
         }
-        if(temp2!=null){
-            temp2.next=null;
-        }
-        temp1.next=head2;
+        odd.next=evenhead;
         return head;
+        
+        
+          // Approach 1:-
+//         if(head==null || head.next==null || head.next.next==null){
+//             return head;
+//         }
+//         ListNode head1=head;
+//         ListNode head2=head.next;
+//         ListNode temp1=head1;
+//         ListNode temp2=head2;
+//         ListNode slow=head;
+//         int count=0;
+//         while(slow!=null){
+//             if(count%2==0){
+//                 if(temp1.next.next!=null){
+//                     temp1.next=temp1.next.next;
+//                     temp1=temp1.next;
+//                 }
+//                 count=1;
+//             }
+//             else{
+//                 temp2.next=temp2.next.next;
+//                 temp2=temp2.next;
+//                 count=0;
+//             }
+//             slow=slow.next;
+            
+//         }
+//         if(temp2!=null){
+//             temp2.next=null;
+//         }
+//         temp1.next=head2;
+//         return head;
     }
 }
