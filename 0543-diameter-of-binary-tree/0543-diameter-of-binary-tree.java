404ef24152e639589ca4544972ebf69091246c55
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2023-01-19 14:31:08 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (94.17%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,19 +14,42 @@
  * }
  */
 class Solution {
-    public static int height(TreeNode root){
-        if(root==null){
-            return -1;
-        }
-        return Math.max(height(root.left),height(root.right))+1;
+            // Approach1 n^2 solution
+    // public static int height(TreeNode root){
+    //     if(root==null){
+    //         return -1;
+    //     }
+    //     return Math.max(height(root.left),height(root.right))+1;
+    // }
+    // public int diameterOfBinaryTree(TreeNode root) {
+    //     if(root==null){
+    //         return 0;
+    //     }
+    //     int ld = diameterOfBinaryTree(root.left);
+    //     int rd = diameterOfBinaryTree(root.right);
+    //     int sd = height(root.left)+height(root.right)+2;
+    //     return Math.max(ld,Math.max(rd,sd));
+    // }
+    
+    
+                // Approach2 n solution
+    class Diapair{
+        int h = -1;
+        int d = 0;
     }
     public int diameterOfBinaryTree(TreeNode root) {
+        return diameter(root).d;
+    }
+    public Diapair diameter(TreeNode root){
         if(root==null){
-            return 0;
+            return new Diapair();
         }
-        int ld = diameterOfBinaryTree(root.left);
-        int rd = diameterOfBinaryTree(root.right);
-        int sd = height(root.left)+height(root.right)+2;
-        return Math.max(ld,Math.max(rd,sd));
+        Diapair ldp = diameter(root.left);
+        Diapair rdp = diameter(root.right);
+        Diapair sdp = new Diapair();
+        int sd = ldp.h+rdp.h+2;
+        sdp.d = Math.max(ldp.d,Math.max(rdp.d,sd));
+        sdp.h = Math.max(ldp.h,rdp.h)+1;
+        return sdp;
     }
 }
