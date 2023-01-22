65baedc11b260d7222710a2a0cf386531144a5c6
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2023-01-22 19:24:30 +0530

    Time: 1 ms (21.36%), Space: 42.7 MB (5.95%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -35,9 +35,6 @@ class Solution {
             }
         }
         return obj;
-        // obj.add(root.val);
-        // preorderTraversal(root.left);
-        // preorderTraversal(root.right);
-        // return obj;
+        
     }
 }
