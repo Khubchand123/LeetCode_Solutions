8641ff86c393c53c4eac5f5a8207c2e576fc473b
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2023-02-25 09:38:16 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (19.05%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -14,20 +14,34 @@ class Solution {
     // }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-           // 1. recursive Approach
+           // 1. recursive Approach TC:O(n) SC:O(2*n)
         // int arr[] = new int[n];
         // int ans = Math.min(solve(cost,n-1,arr),solve(cost,n-2,arr));
         // return ans;
         
-           // 2. Iterative approach
+           // 2. Iterative approach TC:O(n) SC:O(n)
+         
+//         int dp[] = new int[n+1];
+//         dp[0] = cost[0];
+//         dp[1] = cost[1];
         
-        int dp[] = new int[n+1];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+//         for(int i=2;i<n;i++){
+//             dp[i] = Math.min(dp[i-1],dp[i-2])+cost[i];
+//         }
+//         return Math.min(dp[n-1],dp[n-2]);
         
+                // 3. two varaiable approach
+        
+        int prev0 = cost[0];
+        int prev1 = cost[1];
         for(int i=2;i<n;i++){
-            dp[i] = Math.min(dp[i-1],dp[i-2])+cost[i];
+            int temp = cost[i]+Math.min(prev0,prev1);
+            
+            prev0 = prev1;
+            
+            prev1 = temp;
         }
-        return Math.min(dp[n-1],dp[n-2]);
+        
+        return Math.min(prev0,prev1);
     }
 }
