4cda805612b6ade7f42f4b2f0b669f384650970e
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2023-02-25 16:03:17 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (43.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,25 +1,46 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0],nums[1]);
+    int helper(int[] nums,int n){
+        if(n<0){
+            return 0;
+        }
+        if(n==0){
+            return nums[0];
+        }
+        int incl = helper(nums,n-2) + nums[n];
+        int excl = helper(nums,n-1) + 0;
         
-        int dp[] = new int[nums.length];
-        dp[0]=nums[0];
-        if(nums[1]>nums[0]){
-            dp[1]=nums[1];
+        return Math.max(incl,excl);
+    }
+    int helper(int[] nums,int[] dp,int n){
+        if(n<0){
+            return 0;
         }
-        else{
-            dp[1]=nums[0];
+        if(n==0){
+            return nums[0];
         }
-        for(int i=2;i<dp.length;i++){
-            if(nums[i]+dp[i-2]>dp[i-1]){
-                dp[i]=nums[i]+dp[i-2];
-            }
-            else{
-                dp[i]=dp[i-1];
-            }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int incl = helper(nums,dp,n-2) + nums[n];
+        int excl = helper(nums,dp,n-1) + 0;
+        dp[n] = Math.max(incl,excl);
+        return dp[n];
+    }
+    
+    public int rob(int[] nums) {
+        int n = nums.length;
+        
+                // 1. Recursive Approach Brute force
+        
+        // return helper(nums,n-1);
+        
+                  // 2. Recursive Approach Best
+        
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        dp[0]=helper(nums,dp,n-1);
+        if(dp[n-1]==-1){
+            return 0;
         }
         return dp[n-1];
     }
