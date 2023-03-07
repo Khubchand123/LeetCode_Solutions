c0c3abb173e45c67fa1e9d88cffc2adc1a9bac37
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2023-03-07 23:18:54 +0530

    Time: 21 ms (13.08%), Space: 54.1 MB (66.93%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -4,7 +4,7 @@ class Solution {
         if(i>=matrix.length || j>=matrix[0].length){
             return 0;
         }
-        if(dp[i][j]!=-1){
+        if(dp[i][j]!=0){
             return dp[i][j];
         }
         int down = solve(matrix,dp,i+1,j);
@@ -23,9 +23,9 @@ class Solution {
     }
     public int maximalSquare(char[][] matrix) {
         int dp[][] = new int[matrix.length][matrix[0].length];
-        for(int i=0;i<matrix.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
+        // for(int i=0;i<matrix.length;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
         max = 0;
         solve(matrix,dp,0,0);
         return max*max;
