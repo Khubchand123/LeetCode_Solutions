395ff31df09776810a41be5340998eb94f1c6cd2
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2023-03-15 09:55:42 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (27.72%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int i=0;
+        int count = 0;
+        
+        while(miss<=n){
+            if(i<nums.length && miss>=nums[i]){
+                miss+=nums[i];
+                i++;
+            }
+            else{
+                count++;
+                miss+=miss;
+            }
+        }
+        return count;
+    }
+}
