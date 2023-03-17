108ac530b262f202caa482164aabb3236b5e4ab8
Khubchand123 <112496478+Khubchand123@users.noreply.github.com>
2023-03-17 21:32:26 +0530

    Time: 2 ms (100.00%), Space: 42.9 MB (27.42%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,28 +1,27 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
         int ans = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    count=0;
-                    helper(grid,i,j);
-                    ans = Math.max(ans,count);
-                    count=0;
+                    ans = Math.max(ans,helper(grid,m,n,i,j));
                 }
             }
         }
         return ans;
     }
-    static int count;
-    public static void helper(int[][] grid,int sr,int sc){
-        if(sr<0 || sc<0 || sr>=grid.length || sc>=grid[0].length || grid[sr][sc]==0){
-            return;
+    int helper(int[][] grid,int m,int n,int i,int j){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0){
+            return 0;
         }
-        count++;
-        grid[sr][sc] = 0;
-        helper(grid,sr+1,sc);
-        helper(grid,sr-1,sc);
-        helper(grid,sr,sc+1);
-        helper(grid,sr,sc-1);
+        int ans =1;
+        grid[i][j]=0;
+        ans+=helper(grid,m,n,i+1,j);
+        ans+=helper(grid,m,n,i-1,j);
+        ans+=helper(grid,m,n,i,j+1);
+        ans+=helper(grid,m,n,i,j-1);
+        return ans;
     }
 }
